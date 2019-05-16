f1dfade7cfc0de1d76d9c30f931ef0d144fdd688
Benito Buchheim <benito.buchheim@student.hpi.de>
2019-05-16 20:25:14 +0200

    worked on cp2sim and added some folder structure

--- cp2sim/cp2sim.py
+++ cp2sim/cp2sim.py
@@ -7,36 +7,37 @@ INPATH = "../nsys_root"
 # Specify the full outpath
 OUTPATH = "/sys/nsys"
 
+SIMPATH = "../../HistoryUnix/simh/BIN/pdp11"
+SIMINI = "../sim/boot.ini"
+
 # Time between lines
 # Just putting everything in will crash the simulator
-TBL = 0.2
+TBL = 0.1
+
+print("Execution started")
 
 import os, time
 
 commands = []
 def execute(cmd, newline=True):
     end = '\n' if newline else ''
-    print(cmd + end, end='')
+    #print(cmd + end, end='')
     commands.append(cmd + end)
     #time.sleep(TBL)
 
 ######################################
-# SIMULATOR SETUP
+# GATHER COMMANDS
 ######################################
-
-# start subprocess
 
 execute("unix") # choose kernel
 execute("root") # log in
 
-######################################
-# CREATE OUTPATH DIR
-######################################
-
+### CREATE OUTPATH DIRECTORIES ###
 outpathComps = OUTPATH.split("/")
 for i in range(len(outpathComps)-1):
     execute("mkdir " + '/'.join(outpathComps[:(i+2)]))
 
+### CREATE AND FILL FILES AND SUBDIRS ###
 for (root, dirs, files) in os.walk(INPATH):
     simRoot = OUTPATH + root.split(INPATH)[1] + "/"
     
@@ -59,4 +60,14 @@ for (root, dirs, files) in os.walk(INPATH):
         # end cat with control-d
         execute('\n\x04', newline=False)
 
-print(len(commands))
+print ("Will now paste %d lines" % (len(commands)))
+s = len(commands) * TBL
+print ("Will take about %d seconds (%.2f minutes)" % (s, s/60))
+try:
+    input("Continue with ENTER\n")
+except KeyboardInterrupt:
+    print("Aborted")
+
+######################################
+# START SIMULATOR
+######################################
